716ce16f01d2c6d51e463e9bd97823216d9d7b48
Saksham ved <sakshamved111@gmail.com>
2026-04-13 16:23:15 +0530

    Insertion in DOM

--- JS Projects/P1/script.js
+++ JS Projects/P1/script.js
@@ -63,36 +63,115 @@
 
 
 // QUERYSELECTOR
+// var header = document.querySelector('header')
+// header.style.borderBottom = 'solid 4px black'
+// console.log(header);
 
-var header = document.querySelector('header')
-header.style.borderBottom = 'solid 4px black'
-console.log(header);
+// var input = document.querySelector('input');
+// input.value = 'Enter value here'
+// input.placeholder = 'Enter items to Add +'
 
-var input = document.querySelector('input');
-input.value = 'Enter value here'
-input.placeholder = 'Enter items to Add +'
-
-var submit = document.querySelector('input[type="submit"]');
-submit.textContent = "ADD";
-
-
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color = 'yellow';
-
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-
-secondItem.style.color = 'coral';
-
-// QUERYSELECTORALL
-
-var titles = document.querySelectorAll('.title');
-
-titles[0].textContent = 'hello';
+// var submit = document.querySelector('input[type="submit"]');
+// submit.textContent = "ADD";
 
 
-var odd = document.querySelectorAll('li:nth-child(odd)')
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'yellow';
 
-console.log(odd);
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+
+// secondItem.style.color = 'coral';
+
+// // QUERYSELECTORALL
+
+// var titles = document.querySelectorAll('.title');
+
+// titles[0].textContent = 'hello';
+
+
+// var odd = document.querySelectorAll('li:nth-child(odd)')
+
+// console.log(odd);
+
+//========================= TRAVERSING THE DOM==============================
+
+var itemList = document.querySelector('#items')
+// parentnode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = 'grey';
+// console.log(itemList.parentNode.parentNode);
+
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = 'light-grey';
+
+
+// ChildNodes
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor  = 'red';
+
+// FirstChild
+// console.log(itemList.firstChild);
+// FirstElementChild
+// console.log(itemList.firstElementChild);
+// console.log(itemList.firstElementChild.textContent = 'hello 1');
+
+// lastChild
+// console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// console.log(itemList.lastElementChild.textContent = 'helooooooooooooooooooooo');
+
+
+// nextSibling
+// console.log(itemList.nextSibling);
+// nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// PreviousSibiling
+// console.log(itemList.previousSibling);
+// console.log(itemList.previousElementSibling);
+
+// ========================INSERTION IN DOM=======================
+
+// createElement
+var newDiv = document.createElement('div');
+// Add class
+newDiv.className = 'hello1';
+
+// Add id
+newDiv.id = 'hello2'
+
+// Add title
+newDiv.setAttribute('title', 'prabhu');
+
+// create text Node
+var newDivText = document.createTextNode('hello world');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+console.log(newDiv);
+
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(container);
+
+// Add element just before the Selected Node
+container.insertBefore(newDiv, h1);
+newDiv.style.fontSize = '30px';
+
+
+
+
+
+
+
 
 
 
